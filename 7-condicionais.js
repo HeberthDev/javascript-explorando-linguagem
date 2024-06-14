4a2f3347d57f5ac9219264196fa25aa7fc5299a9
HeberthDev <heberthdeveloper@gmail.com>
2024-06-14 08:03:58 -0300

    Operadores lógicos

---
+++ 7-condicionais.js
@@ -0,0 +1,24 @@
+console.log(`Trabalhando com listas`);
+const listaDeDestinos = new Array (
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 17;
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+if (idadeComprador >= 18) {
+    console.log("Comprador maior de idade.");
+    listaDeDestinos.splice(1,1);    
+} else {
+    console.log("Não é maior de idade e não posso vender.")
+}
+
+console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador == 18);
+
