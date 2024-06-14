9f725ea806b9615666dbc9620f5a6cb1ac52796f
HeberthDev <heberthdeveloper@gmail.com>
2024-06-14 07:49:11 -0300

    Criando listas

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,13 +1,18 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 29;
+// const idade = 29;
 let primeiroNome = "Heberth";
 const sobrenome = "Alexandre";
 
 // console.log(nome + " " + sobrenome);
-console.log(nome, sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
 let contador = 0;
 contador = contador + 1;
 const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade + 1;
+console.log(idade);
