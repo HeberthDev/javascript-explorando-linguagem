ee58eaf38e656b79d8d151152e2ac6a7579636cf
HeberthDev <heberthdeveloper@gmail.com>
2024-06-14 08:11:11 -0300

    Ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,19 +6,24 @@ const listaDeDestinos = new Array (
 );
 
 const idadeComprador = 17;
+const estaAcompanhada = true;
+
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
 if (idadeComprador >= 18) {
     console.log("Comprador maior de idade.");
     listaDeDestinos.splice(1,1);    
+} else if (estaAcompanhada) {
+    console.log("Comprador está acampanhado");
+    listaDeDestinos.splice(1,1); 
 } else {
-    console.log("Não é maior de idade e não posso vender.")
+    console.log("Não é maior de idade e não posso vender.");
 }
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
 
