b418c740ba4ba3a71694d16bb8548aab3eb69c97
HeberthDev <heberthdeveloper@gmail.com>
2024-06-17 07:33:33 -0300

    For..

--- 8-loops.js
+++ 8-loops.js
@@ -39,3 +39,15 @@ while (contador < 3) {
 }
 
 console.log("Destino existe: ", destinoExiste);
+
+if (podeComprar && destinoExiste) {
+    console.log("Boa Viagem");
+} else {
+    console.log("Desculpe tivemos um erro!");
+}
+
+for (let cont = 0; cont < 3 ; cont++) {
+    if (listaDeDestinos[contador] == destino) {
+        destinoExiste = true;
+    }
+}
